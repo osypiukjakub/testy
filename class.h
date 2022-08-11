52a221d9e0ea3c86b1a3b5c637f9c7ad56fd6784
Jakub <osypiuk.jakub@gmail.com>
2022-08-11 22:28:28 +0200

    Wrzucam wszystkie pliki z rozwiazaniem likowania przez includowanie class.cpp

---
+++ class.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class Postac
+{
+public:
+
+void wyswietl();
+
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include "class.h"
+#include "class.cpp"
+
+using namespace std;
+
+
+int main()
+{
+
+Postac a;
+a.wyswietl();
+
+
+}
